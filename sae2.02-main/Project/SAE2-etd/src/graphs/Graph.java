441b4b09bf8fd30f5b999b349458170b66755058
Luca Devaux <LucaDev@orange.fr>
2024-05-24 09:41:47 +0200

    Repare allNodes

--- sae2.02-main/Project/SAE2-etd/src/graphs/Graph.java
+++ sae2.02-main/Project/SAE2-etd/src/graphs/Graph.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 public class Graph implements IGraph{
     
     ArrayList<Edge> AllEdges = new ArrayList<>(); 
+    ArrayList<Node> allNodes = new ArrayList<>();
     
     @Override
     public Node addNode() {
