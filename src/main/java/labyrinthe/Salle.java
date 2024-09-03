9f852b6199b4b26427df7d3f473175bc6b1cb949
Sandi Florent <mahafalimaminirina.florent@etu.u-bordeaux.fr>
2024-09-03 15:40:35 +0200

    Created the Salle class, its constructor, attributes and the equals and hashcode methods

---
+++ src/main/java/labyrinthe/Salle.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labyrinthe;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author mflorent
+ */
+public class Salle implements ISalle{
+    
+    public int X;
+    public int Y;
+    public ESalle Type;
+    public Etage Etage;
+    
+    public Salle(int x, int y, ESalle type, Etage etage){
+        X = x;
+        Y = y;
+        Type = type;
+        Etage = etage;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + this.X;
+        hash = 59 * hash + this.Y;
+        hash = 59 * hash + Objects.hashCode(this.Etage);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Salle other = (Salle) obj;
+        if (this.X != other.X) {
+            return false;
+        }
+        if (this.Y != other.Y) {
+            return false;
+        }
+        return Objects.equals(this.Etage, other.Etage);
+    }
+
+    @Override
+    public int getX() {
+        return X;
+    }
+
+    @Override
+    public int getY() {
+        return Y;
+    }
+
+    @Override
+    public ESalle getType() {
+        return Type;
+    }
+
+    @Override
+    public IEtage getEtage() {
+        return Etage;
+    }
+
+    @Override
+    public boolean estAdjacente(ISalle autre) {
+        return false;
+    }
+    
+}
