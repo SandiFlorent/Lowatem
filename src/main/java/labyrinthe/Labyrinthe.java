33b4efadff6dc5b13ba34e644d21b87a7df935a4
Sandi Florent <mahafalimaminirina.florent@etu.u-bordeaux.fr>
2024-09-04 10:07:15 +0200

    Added the Labyrinthe constructor and successfully charged 2 floors

--- src/main/java/labyrinthe/Labyrinthe.java
+++ src/main/java/labyrinthe/Labyrinthe.java
@@ -1,5 +1,7 @@
 package labyrinthe;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import personnages.IPersonnage;
@@ -11,6 +13,19 @@ import personnages.IPersonnage;
 public class Labyrinthe extends ArrayList<ISalle> implements ILabyrinthe {
     
     private IEtage etageCourant = new Etage();
+    
+    public Labyrinthe(){
+        Etage etage1 = new Etage(1);
+        Etage etage2 = new Etage(2);
+        try {
+            etage1.charger("etages/etage1N.txt");
+            etage2.charger("etages/etage2N.txt");
+            this.addAll(etage1);
+            this.addAll(etage2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public Collection<ISalle> sallesAccessibles(IPersonnage heros) {
